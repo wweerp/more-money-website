e98415f404f95e7a690afc29356f9121b2babb4e
zhangqingchen <18352868150@163.com>
2021-07-06 19:10:04 +0800

    deploy

---
+++ precache-manifest.178e264e502b32f455e097afa86ee799.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1256c0f89088d04d6f28",
+    "url": "/more-money-website/css/app.74ac44ae.css"
+  },
+  {
+    "revision": "8924029580f9feb4f1dd736132fbdc2a",
+    "url": "/more-money-website/index.html"
+  },
+  {
+    "revision": "1256c0f89088d04d6f28",
+    "url": "/more-money-website/js/app.a8e79f29.js"
+  },
+  {
+    "revision": "d4462f333a6aec7c953c",
+    "url": "/more-money-website/js/chunk-vendors.8051c366.js"
+  },
+  {
+    "revision": "42bfa4ba9edfb3426e53b3524294dc7f",
+    "url": "/more-money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/more-money-website/robots.txt"
+  }
+]);
